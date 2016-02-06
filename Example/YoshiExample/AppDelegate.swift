2d9bee80b2b3a12322465a44ccd8e86c9d63296d
Michael Campbell <Michael@prolificinteractive.com>
2016-02-05 19:10:08 -0500

    Added SwiftLint to Example Project

--- Example/YoshiExample/AppDelegate.swift
+++ Example/YoshiExample/AppDelegate.swift
@@ -19,13 +19,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    func application(application: UIApplication, didFinishLaunchingWithOptions
+        launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
 
         #if DEBUG
             setupDebugMenu()
         #endif
-        
+
         return true
     }
 
@@ -36,27 +37,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let menuItemQA = MenuItem(name: "QA")
         let environmentItems: [YoshiTableViewMenuItem] = [menuItemProd, menuItemStaging, menuItemQA]
 
-        let tableViewMenu = TableViewMenu(debugMenuName: "Environment", displayItems: environmentItems, didSelectDisplayItem: { (displayItem) in
-            NSNotificationCenter.defaultCenter().postNotificationName(Notifications.EnvironmentUpdatedNotification, object: displayItem.displayText())
+        let tableViewMenu = TableViewMenu(debugMenuName: "Environment",
+            displayItems: environmentItems, didSelectDisplayItem: { (displayItem) in
+            NSNotificationCenter.defaultCenter()
+                .postNotificationName(Notifications.EnvironmentUpdatedNotification, object: displayItem.displayText())
         })
 
         // YoshiMenuType.DateSelector
         let dateSelector = DateSelector(debugMenuName: "Environment Date", didUpdateDate: { (dateSelected) in
-            NSNotificationCenter.defaultCenter().postNotificationName(Notifications.EnvironmentDateUpdatedNotification, object: dateSelected)
+            NSNotificationCenter.defaultCenter()
+                .postNotificationName(Notifications.EnvironmentDateUpdatedNotification, object: dateSelected)
         })
 
         // YoshiMenuType.CustomMenu
-        let setup: () -> () = { Void in
+        let setup: () -> () = {
             print("setup")
         }
 
-        let completion: () -> () = { Void in
+        let completion: () -> () = {
             print("completed")
         }
 
         let customMenu = CustomMenu(debugMenuName: "Custom", setup: setup, completion: completion)
         DebugMenu.startWithInvokeEvent(.ShakeMotion, menuItems: [tableViewMenu, dateSelector, customMenu])
     }
-    
-}
 
+}
