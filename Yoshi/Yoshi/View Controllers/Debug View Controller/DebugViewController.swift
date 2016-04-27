e2555735f3085e1b672a2129a51034320b32493b
htinlinn <linn@prolificinteractive.com>
2016-04-27 11:43:54 -0400

    Updated date selector and table view menus to have default subtitles

--- Yoshi/Yoshi/View Controllers/Debug View Controller/DebugViewController.swift
+++ Yoshi/Yoshi/View Controllers/Debug View Controller/DebugViewController.swift
@@ -131,8 +131,25 @@ extension DebugViewController: UITableViewDataSource {
 
         let option = options[indexPath.row]
         cell.textLabel?.text = option.title
-        cell.detailTextLabel?.text = option.subtitle
-
+        
+        if let subtitle = option.subtitle {
+            cell.detailTextLabel?.text = subtitle
+        } else {
+            switch option {
+            case let dateSelectorMenu as YoshiDateSelectorMenu:
+                cell.detailTextLabel?.text = dateSelectorMenu.selectedDate.description
+            case let tableViewMenu as YoshiTableViewMenu:                
+                for displayItem in tableViewMenu.displayItems {
+                    if displayItem.selected == true {
+                        cell.detailTextLabel?.text = displayItem.name
+                        break
+                    }
+                }
+            default:
+                cell.detailTextLabel?.text = nil
+            }
+        }
+        
         switch option {
         case _ as YoshiDateSelectorMenu:
             cell.accessoryType = .DisclosureIndicator
