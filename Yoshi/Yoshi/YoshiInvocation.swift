cca9d0eae1fe5d86dfea1b2686b784468fbd751d
Quentin RIBIERRE <quentin@prolificinteractive.com>
2016-10-24 19:10:32 -0400

    [Bug] Shake Gesture broken on iOS 10
    [Shake Gesture broken on iOS 10 GM](https://github.com/prolificinteractive/Yoshi/issues/50) #50

---
+++ Yoshi/Yoshi/YoshiInvocation.swift
@@ -0,0 +1,34 @@
+//
+//  YoshiInvocation.swift
+//  Pods
+//
+//  Created by Quentin Ribierre on 10/24/16.
+//
+//
+
+import Foundation
+
+/// Set of Yoshi invocation method.
+public struct YoshiInvocation: OptionSetType {
+
+    /// An `OptionSet`'s `Element` type is normally `Self`.
+    public let rawValue: UInt64
+
+    /// Convert from a value of `RawValue`, succeeding unconditionally.
+    public init(rawValue: UInt64) {
+        self.rawValue = rawValue
+    }
+
+    /// All invocations types.
+    public static let all = YoshiInvocation(rawValue: 0)
+
+    /// Shake motion gesture invocation type.
+    public static let shakeMotionGesture = YoshiInvocation(rawValue: 1 << 0)
+
+    /// Multi touch event invocation type.
+    public static let multiTouch = YoshiInvocation(rawValue: 1 << 1)
+
+    /// Force touch event invocation type.
+    public static let forceTouch = YoshiInvocation(rawValue: 1 << 2)
+
+}
