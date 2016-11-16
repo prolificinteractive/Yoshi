3e6a6fcf41fa28ee84d335f0a9b1dd9a933a65c1
Quentin RIBIERRE <quentin@prolificinteractive.com>
2016-11-16 14:09:16 -0500

    Update Yoshi version and minimum touch and force logic

--- Yoshi/Yoshi/Utility/UIWindowExtension.swift
+++ Yoshi/Yoshi/Utility/UIWindowExtension.swift
@@ -12,16 +12,32 @@ import UIKit
 // MARK: - UIWindow Extension.
 public extension UIWindow {
 
+    /// Function returning the amount of touch required to show Yoshi from a mulitple touch.
+    /// Override this function to update the value.
+    ///
+    /// - Returns: count of touch required to show Yoshi.
+    public func yoshiTouchesBeganMinimumTouchRequirement() -> Int {
+        return 3
+    }
+
+    /// Function returning the amount of force required to show Yoshi from a force touch.
+    /// Override this function to update the value.
+    ///
+    /// - Returns: value of force required to show Yoshi.
+    public func yoshiTouchesMovedMinimumForcePercent() -> Float {
+        return 60
+    }
+
     override open func motionBegan(_ motion: UIEventSubtype, with event: UIEvent?) {
         Yoshi.motionBegan(motion, withEvent: event)
     }
 
     override open func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        Yoshi.touchesBegan(touches, withEvent: event)
+        Yoshi.touchesBegan(touches, withEvent: event, minimumTouchRequirement: yoshiTouchesBeganMinimumTouchRequirement())
     }
 
     override open func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        Yoshi.touchesMoved(touches, withEvent: event)
+        Yoshi.touchesMoved(touches, withEvent: event, minimumForcePercent: yoshiTouchesMovedMinimumForcePercent())
     }
 
 }
